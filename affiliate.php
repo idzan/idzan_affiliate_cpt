e0c47231cdd09a72ae1b41e0eb0596723ed12d0f
Marko Idzan <marko@idzan.eu>
2023-05-17 22:16:32 +0200

    Update affiliate.php
    minor fix

--- affiliate.php
+++ affiliate.php
@@ -68,38 +68,3 @@ function idzan_affiliate_cpt() {
 
 }
 add_action( 'init', 'idzan_affiliate_cpt', 0 );
-
-/* Taxonomy for Affiliates - Services of Affiliates */
-function idzan_affiliate_Type() {
-	$labels = array(
-		'name'              => _x( 'Affiliate type', 'taxonomy general name', 'idzan-affiliate-Type' ),
-		'singular_name'     => _x( 'Affiliate Type', 'taxonomy singular name', 'idzan-affiliate-Type' ),
-		'search_items'      => __( 'Search Affiliate type', 'idzan-affiliate-Type' ),
-		'all_items'         => __( 'All Affiliate type', 'idzan-affiliate-Type' ),
-		'parent_item'       => __( 'Parent Affiliate Type', 'idzan-affiliate-Type' ),
-		'parent_item_colon' => __( 'Parent Affiliate Type:', 'idzan-affiliate-Type' ),
-		'edit_item'         => __( 'Edit Affiliate Type', 'idzan-affiliate-Type' ),
-		'update_item'       => __( 'Update Affiliate Type', 'idzan-affiliate-Type' ),
-		'add_new_item'      => __( 'Add New Affiliate Type', 'idzan-affiliate-Type' ),
-		'new_item_name'     => __( 'New Affiliate Type Name', 'idzan-affiliate-Type' ),
-		'menu_name'         => __( 'Affiliate Type', 'idzan-affiliate-Type' ),
-	);
-    
-	$args = array(
-		'labels' => $labels,
-		'description' => __( 'Type of Affiliate Link (Netwroking, VPN, Security, Hosting etc)', 'idzan-affiliate-Type' ),
-		'hierarchical' => false,
-		'public' => true,
-		'publicly_queryable' => true,
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'show_in_nav_menus' => true,
-		'show_tagcloud' => true,
-		'show_in_quick_edit' => true,
-		'show_admin_column' => false,
-		'show_in_rest' => true,
-	);
-	register_taxonomy( 'affiliatetype', array('affiliate'), $args );
-
-}
-add_action( 'init', 'idzan_affiliate_Type' );
